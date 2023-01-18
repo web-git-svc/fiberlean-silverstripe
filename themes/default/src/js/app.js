3c9ed6c56a5e27596fe54a5bf857ef12784868ee
Loz Calver <lozcalver@bigfork.co.uk>
2023-01-18 12:05:49 +0000

    Make hero block a carousel

--- themes/default/src/js/app.js
+++ themes/default/src/js/app.js
@@ -5,6 +5,7 @@
 
 import './classes/StickyHeader';
 import {ElementFullWidthCarousel}         from './components/ElementFullWidthCarousel';
+import {ElementHero}                      from './components/ElementHero';
 import {NavMenuHandler, OffCanvasHandler} from './components/Navigation';
 import {ElementSlideshow}                 from "./components/ElementSlideshow";
 import {FluentDropdown}                   from './components/FluentDropdown';
@@ -12,6 +13,7 @@ import {FluentDropdown}                   from './components/FluentDropdown';
 const init = _ => {
   FluentDropdown();
   ElementFullWidthCarousel();
+  ElementHero();
   ElementSlideshow();
   NavMenuHandler();
   OffCanvasHandler();
